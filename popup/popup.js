99ac9b02d543f77c81b6c4c9dae2794de176931b
AmanB779 <amanofficial9903@gmail.com>
2024-09-23 14:23:08 +0530

    Extension prod ready

---
+++ popup/popup.js
@@ -0,0 +1,4 @@
+document.getElementById("submitButton").addEventListener("click", () => {
+  const userInput = document.getElementById("userInput").value;
+  alert(`User message: ${userInput}`);
+});
